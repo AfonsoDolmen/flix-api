7e9e8fd6a7aaa424fa7941dc825c5f962a0e9aff
AfonsoDolmen <quadradoplay@outlook.com>
2025-01-21 21:42:01 -0300

    Alterado validações.

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,13 +3,18 @@ from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
 
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        return 5
+
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError(
                 'A data de lançamento não pode ser anterior a 1990.'
             )
@@ -17,7 +22,7 @@ class MovieSerializer(serializers.ModelSerializer):
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError(
                 'O campo resumo não pode ser maior que 200 caracteres.'
             )
