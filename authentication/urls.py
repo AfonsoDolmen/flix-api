3da85a0863e23695bd036a66f40d035c8e32a7a8
AfonsoDolmen <quadradoplay@outlook.com>
2025-01-27 19:35:06 -0300

    fix - Ajuste de nomenclatura.

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('authentication/token/', TokenObtainPairView.as_view(), name='obatin-pair-token'),
+    path('authentication/token/', TokenObtainPairView.as_view(), name='obtain-pair-token'),
     path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('authentication/token/verify/', TokenVerifyView.as_view(), name='token-verify'),
 ]
