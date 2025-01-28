161aefd5a6d5d50ffc3ceabb21cdd474d4a009b7
AfonsoDolmen <quadradoplay@outlook.com>
2025-01-27 22:42:41 -0300

    Implementado rotas de métricas para movies.

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
+from movies.views import (
+    MovieListCreateView,
+    MovieRetrieveUpdateDestroyView,
+    MovieStatsView
+)
+
 
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats'),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, views, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import response
+from django.db.models import Count, Avg
 from movies.serializers import MovieSerializer
 from movies.models import Movie
+from reviews.models import Review
 from app.permissions import GlobalDefaultPermission
 
 
@@ -15,3 +18,22 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        # Busca todos os dados
+        total_movies = self.queryset.count()
+        total_movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        reviews_average = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        return response.Response({
+            'total_movies': total_movies,
+            'total_movies_by_genre': total_movies_by_genre,
+            'total_reviews': total_reviews,
+            'reviews_average': reviews_average,
+        },status.HTTP_200_OK)
